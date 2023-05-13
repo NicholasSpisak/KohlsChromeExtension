0a83e12ebd855dbb90b683ec3f7dccff4eade40c
spisakni <nicholas.r.spisak@gmail.com>
2023-05-13 18:29:34 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css'; // Import custom CSS styles
 
 const App = () => {
   const [upc, setUpc] = useState('');
@@ -13,25 +14,32 @@ const App = () => {
     const apiUrl = `https://bqke5kdb94.execute-api.us-east-2.amazonaws.com/Prod/kohls/product?upc=${upc}`;
 
     fetch(apiUrl)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('API call failed');
+      })
       .then((data) => {
         setApiResponse(data);
         setError(null);
       })
       .catch((error) => {
-        setError('API call error');
+        setError(error.message);
         setApiResponse(null);
       });
   };
 
   return (
-    <div>
+    <div className="app">
       <h1>Kohls Chrome Extension</h1>
-      <input type="text" value={upc} onChange={handleUpcChange} />
-      <button onClick={handleApiCall}>Run API Call</button>
+      <div className="input-container">
+        <input type="text" value={upc} onChange={handleUpcChange} placeholder="Enter UPC" />
+        <button onClick={handleApiCall}>Run API Call</button>
+      </div>
 
       {apiResponse && (
-        <div>
+        <div className="api-response">
           <h2>API Response:</h2>
           <p>Product Name: {apiResponse.product_name}</p>
           <p>Brand: {apiResponse.brand}</p>
@@ -44,7 +52,7 @@ const App = () => {
         </div>
       )}
 
-      {error && <p>Error: {error}</p>}
+      {error && <p className="error">Error: {error}</p>}
     </div>
   );
 };
