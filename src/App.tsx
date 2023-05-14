7f725377a038613a3ceeb420d0aaac5e94a37915
spisakni <nicholas.r.spisak@gmail.com>
2023-05-13 21:10:57 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import './App.css'; // Import custom CSS styles
 
 const App = () => {
-  const [upc, setUpc] = useState('');
+  const [web_id, setWebId] = useState('');
   const [apiResponse, setApiResponse] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const handleUpcChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUpc(event.target.value);
+  const handleWebIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setWebId(event.target.value);
   };
 
   const handleApiCall = () => {
-    const apiUrl = `https://bqke5kdb94.execute-api.us-east-2.amazonaws.com/Prod/kohls/product?upc=${upc}`;
+    const apiUrl = `https://ocektojzh9.execute-api.us-east-2.amazonaws.com/Prod/kohls/product?web_id=${web_id}`;
 
     fetch(apiUrl)
       .then((response) => {
@@ -34,14 +34,16 @@ const App = () => {
     <div className="app">
       <h1>Kohls Chrome Extension</h1>
       <div className="input-container">
-        <input type="text" value={upc} onChange={handleUpcChange} placeholder="Enter UPC" />
-        <button onClick={handleApiCall}>Run API Call</button>
+        <input type="text" value={web_id} onChange={handleWebIdChange} placeholder="Enter WebID" />
+        <button onClick={handleApiCall}>Run</button>
       </div>
 
       {apiResponse && (
         <div className="api-response">
           <h2>API Response:</h2>
           <p>Product Name: {apiResponse.product_name}</p>
+          <p>Sale Price: {apiResponse.sale_price}</p>
+          <p>Regular Price: {apiResponse.regular_price}</p>
           <p>Brand: {apiResponse.brand}</p>
           <p>Category: {apiResponse.category}</p>
           <p>Color: {apiResponse.color}</p>
